f7d8a8075f95e7a029b18db1348e8f85b2999ba7
Morten Bøgh <mob@trifork.com>
2013-12-09 23:54:47 +0100

    Added a "currency" helper to do pretty displaying of the prices. Simple two digit display

--- js/application.js
+++ js/application.js
@@ -1,3 +1,8 @@
 window.Diapers = Ember.Application.create();
 
 Diapers.ApplicationAdapter = DS.FixtureAdapter.extend();
+
+Ember.Handlebars.helper('currency', function(value, options) {
+  var escaped = Handlebars.Utils.escapeExpression(value);
+  return new Handlebars.SafeString(parseFloat(Math.round(escaped * 100) / 100).toFixed(2));
+});
